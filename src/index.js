0604cc682e0b7c2fce507d4dc5b98e402af996b4
Viyookan <jviyookan@yahoo,com>
2023-10-04 10:06:17 +0530

    removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-// export this package's api
-import createForm from './createForm';
-import createFormField from './createFormField';
-import formShape from './propTypes';
 
-export { createFormField, formShape, createForm };
